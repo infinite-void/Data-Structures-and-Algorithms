b5a4850fce20659194e76a643a190890f70db0e2
infinite-void <srinathsk18@gmail.com>
2020-06-14 14:25:25 +0530

    update naive.cpp

--- Strings/naive.cpp
+++ Strings/naive.cpp
@@ -7,15 +7,25 @@ int main() {
     
     string main_string, pattern;
     
-    cout << "Enter the main string";
+    cout << "Enter the main string :";
     cin >> main_string;
     
-    cout << "Enter the pattern";
+    cout << "Enter the pattern :";
     cin >> pattern;
     
     int main_length = main_string.size();
     int pattern_length = pattern.size();
     
+/* The variable i always denotes the starting index of a substring
+ * when when search for a pattern over a string.
+ * 
+ * The variable j is used to traverse over the maximum length of the 
+ * substring pattern upto which the pattern is present in the 
+ * substring that starts with the corresponding i.
+ * 
+ * If j == pattern_length the substring itself resembles the pattern
+ * and the pattern is present starting at index i
+ */
     for(int i = 0; i <= main_length - pattern_length; i++) {
         int j;
         for(j = 0; j < pattern_length; j++)
