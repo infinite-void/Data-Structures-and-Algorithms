d38b83e18dd44446d4ea9c88d6c64914f3d2852c
infinite-void <srinathsk18@gmail.com>
2020-06-15 12:30:11 +0530

    update quicksort and mergesort

--- Divide&Conquer/mergesort.c
+++ Divide&Conquer/mergesort.c
@@ -10,6 +10,12 @@ int main() {
 	scanf("%d", &n);
 	
 	int* array = (int*) calloc(n, sizeof(int));
+	
+	if(!array) {
+		fprintf(stderr, "Could not allocate memory. Terminating. \n");
+		return -1;
+	}
+
 	for(i = 0; i < n; i++) 
 		scanf("%d", &array[i]);
 
@@ -21,7 +27,19 @@ int main() {
 	free(array);
 	return 0;
 }
-
+/* The mergesort() function implements the
+ * divide and conquer approach where the array
+ * is divided into 2 halves and recursed.
+ * This recursion goes on till the array has only 
+ * one element. An array with one element is self-sorted 
+ * returns without any operations.
+ * The merge operation merges the left and right parts of 
+ * the array in linear time.
+ * This way mergesort() divides the array on it way down 
+ * to an input array of one element and merges back in
+ * sorted order on its way up!
+ * Hence, Divided and Conquered
+ */
 void mergeSort(int* array, int left, int right) {
 	if(left >= right)
 		return;
@@ -29,13 +47,23 @@ void mergeSort(int* array, int left, int right) {
 	int mid = (left + right) / 2;
 	mergeSort(array, left, mid);
 	mergeSort(array, mid + 1, right);
+	
 	merge(array, left, mid, right);
 }
 
+/*The merge operation uses temporary memory to merge
+ * the 2 sorted parts of the array
+ */
+
 void merge(int* array, int left, int mid, int right) {
 	int size = right - left + 1;
-	int* temp = (int*) calloc(size, sizeof(int));
 	int i = left, j = mid + 1, k = 0;
+	int* temp = (int*) calloc(size, sizeof(int));
+	
+	if(!temp) {
+		fprintf(stderr, "Error in allocating temporary memory. \n");
+		exit(-1);
+	}
 	
 	while(i <= mid && j <= right) {
 		if(array[i] < array[j]) {
--- Divide&Conquer/quicksort.c
+++ Divide&Conquer/quicksort.c
@@ -11,6 +11,12 @@ int main() {
 	scanf("%d", &n);
 	
 	int* array = (int*)calloc(n , sizeof(int));
+	
+	if(!array) {
+		fprintf(stderr, "Error in memory allocation for array. \n");
+		return -1;
+	}
+
 	for(i = 0; i < n; i++) 
 		scanf("%d", &array[i]);
 	quickSort(array, 0, n - 1);
@@ -23,6 +29,20 @@ int main() {
 	return 0;
 }
 
+
+/* The quicksort employs divide and conquer approach 
+ * such that it partitions the given array based on
+ * a pivot element (partition may or may not be of 
+ * equal sizes). The elements to the left of the pivot 
+ * element are less than the pivot element and those on 
+ * the right are greater than or equal to it.
+ * Now, the left and right partitions are sorted recursively
+ * based on partitioning.
+ * Arbitrarily, the right most element is always chosen as pivot.
+ * This may have an worst case time complexity of O(n^2).
+ * Randomized QuickSort may make this worst case scenario almost improbable.
+ */
+
 void quickSort(int* array, int left, int right) {
 	if(left >= right)
 		return;
@@ -30,6 +50,10 @@ void quickSort(int* array, int left, int right) {
 	quickSort(array, left, partedIndex - 1);
 	quickSort(array, partedIndex + 1, right);
 }
+
+/* Partition the array into 2 parts and return 
+ * index of the pivot element.
+ */
 
 int partition(int* array, int left, int right) {
 	int pivot = array[right];
